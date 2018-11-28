91f261bcfa4a90d392a632bd0f325f73991a4a96
Aleksandr Spiridonov <sasha.aos@gmail.com>
2018-11-27 20:09:56 -0500

    added domain ref to csv transaction repo

--- src/Infrastructure/Repositories/CSVTransactionRepository/CSVTransactionRepository.cs
+++ src/Infrastructure/Repositories/CSVTransactionRepository/CSVTransactionRepository.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using Domain;
+using Domain.Repositories;
 
 namespace CSVTransactionRepository
 {
-    public class CSVTransactionRepository
+    public class TransactionRepository : ITransactionRepository
     {
+        public List<Transaction> GetAll()
+        {
+            return new List<Transaction>();
+        }
+
+        public List<Transaction> GetAllWhere(Func<Transaction, bool> filter)
+        {
+            return new List<Transaction>();
+        }
     }
 }
