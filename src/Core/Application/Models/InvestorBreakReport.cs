792b2ca2ba4114582ca5c23e18ad2a4995f7f917
Aleksandr Spiridonov <sasha.aos@gmail.com>
2018-11-28 21:41:19 -0500

    added Fund property to and removed CashImbalance property from Investor Break Report model

--- src/Core/Application/Models/InvestorBreakReport.cs
+++ src/Core/Application/Models/InvestorBreakReport.cs
@@ -5,23 +5,22 @@ namespace Application.Models
     public class InvestorBreakReport
     {
         public Investor Investor { get; set; }
+        public Fund Fund { get; set; }
         public double ShareImbalance { get; set; }
-        public double CashImbalance { get; set; }
 
         public override bool Equals(object obj)
         {
             var other = obj as InvestorBreakReport;
             if (other == null) return false;
             return this.Investor.Equals(other.Investor) &&
-                    this.ShareImbalance.Equals(other.ShareImbalance) &&
-                    this.CashImbalance.Equals(other.CashImbalance);
+                    this.Fund.Equals(other.Fund) &&
+                    this.ShareImbalance.Equals(other.ShareImbalance);
         }
 
         public override int GetHashCode()
         {
             return this.Investor.GetHashCode() ^
-                    this.ShareImbalance.GetHashCode() ^
-                    this.CashImbalance.GetHashCode();
+                    this.Fund.GetHashCode();
         }
     }
 }
