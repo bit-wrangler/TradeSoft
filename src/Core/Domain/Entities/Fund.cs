a97555dc327dd1e10175f411cdd90c979a494308
Aleksandr Spiridonov <sasha.aos@gmail.com>
2018-11-28 20:49:28 -0500

    added overrides for Equals for Investor and Fund entities

--- src/Core/Domain/Entities/Fund.cs
+++ src/Core/Domain/Entities/Fund.cs
@@ -3,5 +3,12 @@ namespace Domain
     public class Fund
     {
         public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Fund;
+            if (other == null) return false;
+            return this.Name.Equals(other.Name);
+        }
     }
 }
--- src/Core/Domain/Entities/Investor.cs
+++ src/Core/Domain/Entities/Investor.cs
@@ -3,5 +3,12 @@ namespace Domain
     public class Investor
     {
         public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Investor;
+            if (other == null) return false;
+            return this.Name.Equals(other.Name);
+        }
     }
 }
