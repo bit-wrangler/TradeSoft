cc03764195a33bf4c2a509c32e19e8e785e5c4ce
Aleksandr Spiridonov <sasha.aos@gmail.com>
2018-11-28 19:15:19 -0500

    Added model for AUMSummary

---
+++ src/Core/Application/Models/SalesPersonAUMSummary.cs
@@ -0,0 +1,10 @@
+using Domain;
+
+namespace Application.Models
+{
+    public class SalesPersonAUMSummary
+    {
+        public SalesPerson SalesPerson { get; set; }
+        public double Amount { get; set; }
+    }
+}
